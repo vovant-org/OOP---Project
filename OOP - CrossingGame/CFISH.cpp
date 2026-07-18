50924963d7da33061edb9a3bd8ae0838a00c4b53
namvuthanh15 <namvuthanh38@gmail.com>
2026-07-18 16:54:38 +0700

    update obstacle

--- OOP - CrossingGame/CFISH.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CANIMAL.h"
-
-class CFISH : public CANIMAL {
-public:
-    CFISH(float startX, float startY, float spd, bool moveRight);
-
-    void Update(float deltaTime) override;
-};
